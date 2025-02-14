67c5f80da1e0edba8a5b05db5f2d53f7815d1b50
NBN-Barath <1barathnivash@gmail.com>
2025-02-14 11:02:30 +0530

    Delete src directory

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws CloneNotSupportedException{
-        //ATM atm = new ATM();
-        ATM_Action.start();// call the start method in ATM
-    }
-}
